9b0277331b7fb5bcf4560bb9ac7ea27d38dd7b0a
JH <ehwjh2010@aliyun.com>
2022-02-23 21:10:33 +0800

    完善单测

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ehwjh2010/viper
 go 1.17
 
 require (
+	github.com/apolloconfig/agollo/v4 v4.1.0
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/locales v0.14.0
@@ -15,6 +16,7 @@ require (
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/panjf2000/ants/v2 v2.4.7
 	github.com/pkg/errors v0.9.1
+	github.com/smartystreets/goconvey v1.7.2
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
 	github.com/swaggo/gin-swagger v1.3.3
 	go.mongodb.org/mongo-driver v1.8.1
@@ -29,7 +31,6 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
-	github.com/apolloconfig/agollo/v4 v4.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
@@ -67,7 +68,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/smartystreets/assertions v1.2.0 // indirect
-	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/spf13/afero v1.8.1 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -83,13 +83,11 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
-	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.5 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
--- helper/equal/equal_test.go
+++ helper/equal/equal_test.go
@@ -20,7 +20,87 @@ func TestSliceBytesEqual(t *testing.T) {
 		}
 
 		for _, test := range tests {
-			convey.So(SliceBytesEqual(test.Value, test.Dest), convey.ShouldEqual, test.OK)
+			convey.So(SliceByteEqual(test.Value, test.Dest), convey.ShouldEqual, test.OK)
+		}
+	})
+}
+
+func TestSliceStrEqual(t *testing.T) {
+	convey.Convey("[]string equal", t, func() {
+		tests := []struct {
+			Value []string
+			Dest  []string
+			OK    bool
+		}{
+			{nil, []string{"1"}, false},
+			{nil, nil, true},
+			{[]string{"1"}, []string{"1", "2"}, false},
+			{[]string{"1", "3"}, []string{"1", "2"}, false},
+			{[]string{"1", "3"}, []string{"1", "3"}, true},
+		}
+
+		for _, test := range tests {
+			convey.So(SliceStrEqual(test.Value, test.Dest), convey.ShouldEqual, test.OK)
+		}
+	})
+}
+
+func TestSliceIntEqual(t *testing.T) {
+	convey.Convey("[]int equal", t, func() {
+		tests := []struct {
+			Value []int
+			Dest  []int
+			OK    bool
+		}{
+			{nil, []int{1}, false},
+			{nil, nil, true},
+			{[]int{1}, []int{1, 2}, false},
+			{[]int{1, 3}, []int{1, 2}, false},
+			{[]int{1, 3}, []int{1, 3}, true},
+		}
+
+		for _, test := range tests {
+			convey.So(SliceIntEqual(test.Value, test.Dest), convey.ShouldEqual, test.OK)
+		}
+	})
+}
+
+func TestSliceInt32Equal(t *testing.T) {
+	convey.Convey("[]int32 equal", t, func() {
+		tests := []struct {
+			Value []int32
+			Dest  []int32
+			OK    bool
+		}{
+			{nil, []int32{1}, false},
+			{nil, nil, true},
+			{[]int32{1}, []int32{1, 2}, false},
+			{[]int32{1, 3}, []int32{1, 2}, false},
+			{[]int32{1, 3}, []int32{1, 3}, true},
+		}
+
+		for _, test := range tests {
+			convey.So(SliceInt32Equal(test.Value, test.Dest), convey.ShouldEqual, test.OK)
+		}
+	})
+}
+
+func TestSliceInt64Equal(t *testing.T) {
+	convey.Convey("[]int32 equal", t, func() {
+		tests := []struct {
+			Value []int64
+			Dest  []int64
+			OK    bool
+		}{
+			{nil, []int64{1}, false},
+			{nil, nil, true},
+			{[]int64{1}, []int64{1, 2}, false},
+			{[]int64{1, 3}, []int64{1, 2}, false},
+			{[]int64{1, 3}, []int64{1, 3}, true},
+		}
+
+		for _, test := range tests {
+			convey.So(SliceInt64Equal(test.Value, test.Dest), convey.ShouldEqual, test.OK)
 		}
 	})
 }
